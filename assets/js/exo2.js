96b2d0b8ff816e7af528e2d2fce8e282cb0d5335
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-10-28 09:13:49 +0100

    Create exo2.js

---
+++ assets/js/exo2.js
@@ -0,0 +1,10 @@
+var exo2= document.getElementById("exo2");
+exo2.addEventListener("click",eva2);
+function eva2(){
+    let N=parseInt(prompt("entrer le nombre butée"));
+    let SOM=0
+for(i=0;i<=N;i++){
+SOM+=i;
+}
+alert("la sommes des nombres jusque: "+N+" est egale à: "+SOM);
+}
