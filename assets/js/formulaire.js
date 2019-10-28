099e0df0579c1bb546e6cb0579b7fcd4d0cea7d2
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-10-28 10:54:01 +0100

    Update formulaire.js

--- assets/js/formulaire.js
+++ assets/js/formulaire.js
@@ -8,8 +8,8 @@ function verif(event){
     var ad=document.getElementById("adresse");
     var city= document.getElementById("ville");
     var mail= document.getElementById("email");
-    var filtre=new RegExp(/^.+[a-z|A-Z|0-9]+$/);// creation du filtre "au moins 1 lettre ou un chiffre et caractere speciaux"
-    var filtre1=new RegExp(/^((?:[013-9]\d)|(?:2[0-9ABab]))\d{3}$/)// filtre du code postale il doit contenir 5 nombres sauf la corse qui contien des lettres
+    var filtre=/^[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœÉ]+(?:(?:\-| )?[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœÉ]+)*$/;// creation du filtre "au moins 1 lettre ou un chiffre et caractere speciaux"
+    var filtre1=(/^((?:[013-9]\d)|(?:2[0-9ABab]))\d{3}$/)// filtre du code postale il doit contenir 5 nombres sauf la corse qui contien des lettres
     var filtre2=new RegExp(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i);// filtre email il divise le mail en 4 blocs le 1er accepte lettre, chiffre et caractere le 2nd @ obligatoire 3em pour la sone de texte et le 4em demande un .et des caractéres 
     var filtre4=new RegExp(/^(\d+)(\D*)(\s)(\D+)(\s)(.+)$/g)// filtre adresse il divise en 6 bloc don le 1er chiffre obligatoire le second pour bis (facultatif) un espace obligatoire la voie /rue ou autre un autre espace obligatoire et le reste de l adresse
     var missNom=document.getElementById("missnom");
@@ -19,28 +19,34 @@ function verif(event){
     var missAd=document.getElementById("missAd");
     var missCity=document.getElementById("missCity");
     var missEmail=document.getElementById("missEmail");
-  
-    if(!filtre.test(nom.value)){// on applique le filtre sur la value de la variable ent 
+    
+    if(nom.validity.valueMissing){
+    event.preventDefault();
+    missNom.textContent="champs vide"
+    missNom.style.color="yellow"
+}
+    else if(!filtre.test(nom.value)){// on applique le filtre sur la value de la variable ent 
         event.preventDefault();// si erreur le formulaire et bloqué avec event.preventdefault
-        missNom.textContent= "veuillez saisir le nom votre nom";// text content change le texte entre >< dune balise html
+        missNom.textContent= "veuillez saisir des lettres";// text content change le texte entre >< dune balise html
         missNom.style.color="red"; //si on le desire on peut changer la couleur du text comme l'exemple pour le vert (mis en commentaire car j'utilise une feuille css)
     }
-    else if(nom.validity.valueMissing){
-        event.preventDefault();
-        missNom.textContent="champs vide"
-        missNom.style.color="orange"
-
-     
-    }else{
+    else{
         missNom.textContent= "OK";// si la condition est rempli je remet a l'origine
-        missNom.style.color="red";
+        missNom.style.color="green";
     }
-    if(!filtre.test(per.value)){
+    if(prenom.validity.valueMissing){
         event.preventDefault();
-        missPer.textContent= "veuillez saisir le nom de la personne a contacter";
+        missPre.textContent= "champ vide";
+        missPre.style.color="yellow"
       }
-      else{
-        missPer.textContent= "*";
+      else if(!filtre.test(prenom.value)){
+        event.preventDefault();
+        missPre.textContent="veuillez saisir des lettres"
+        missPre.style.color="orange"
+    }
+    else{
+        missPre.textContent= "OK";
+        missPre.style.color="green";
       }
     if(!filtre.test(city.value)){
         event.preventDefault();
@@ -77,13 +83,5 @@ function verif(event){
     else{
       missTel.textContent= "*";
     }
-}
-var ch1=document.getElementById("ch1");
-ch1.addEventListener("click",txt);// je declanche le 2nd evenement quand je clique sur la barre de selection
-function txt(){
-    var op1=document.getElementById("op1");
-       if(ch1.value==0){//cache la 1ere option
-           op1.innerHTML="";
-        }
-        ch2.innerHTML=ch1.value// si je clique sur une option elle vien s'afficher dans le txtaera
+    console.log(ddn)
 }
