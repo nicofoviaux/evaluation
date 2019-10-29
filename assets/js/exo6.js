84bb3a424dc456c4f88135ead81b6fe6a6bb8717
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-10-29 08:28:15 +0100

    Update exo6.js

--- assets/js/exo6.js
+++ assets/js/exo6.js
@@ -5,11 +5,11 @@ function eva6(){
     let nom= prompt("entré un des prenom ");
     let pos=tab.indexOf(nom);
     console.log(pos);
-    if(pos==-1){
+    if(pos==-1){// -1 car si indexof ne trouve pas il son value es -1
         alert("le nom que vous avez saisie n'est pas dans la liste");
     }else{
-       tab.splice(pos,1);
-       tab.push(" ");
+       tab.splice(pos,1);// suprime 1 case a la position de inde off
+       tab.push(" ");// recree une case vide a la fin
     }
     console.log(tab);
 }
