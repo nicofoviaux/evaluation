843e17451eccd5fec5d3094e14341aa826113c80
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:28:24 +0100

    Create test4.7.js

---
+++ exo/assest/js/1ere partie/test4.7.js
@@ -0,0 +1,14 @@
+var A = parseInt(prompt("entrer un nombre"));
+var prem = 0
+var i = 2
+
+for (i = 2; i <= (Math.sqrt(A)); i++) {//math.sqrt = racine carré en js
+    if (A % i == 0) {
+        prem++;//si le nombre n'est pas premier la variable prem va incrementé
+    }
+}
+if (prem >= 1) {
+    alert("le nombre n'est pas premier!");
+} else {
+    alert("le nombre est premier!");
+}
