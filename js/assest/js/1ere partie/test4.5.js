caaa7e4dac5160e56527feb802637f3110503f80
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:30:02 +0100

    Delete test4.5.js

--- js/assest/js/1ere partie/test4.5.js
+++
@@ -1,9 +0,0 @@
-var A=parseInt(prompt("entrer un nombre")); 
-var B=parseInt(prompt("entrer un nombre"));
-var res=0
-i=1
-do{//do.. while pour injecté les condition av le while
-    res=A*i;
-    alert(A+"*"+i+"="+res);
-    i++;
-}while(i<=B);
