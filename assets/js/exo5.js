f0d90c83624548a60acc64f5503fa82be07596fa
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:21:38 +0100

    Update exo5.js

--- assets/js/exo5.js
+++ assets/js/exo5.js
@@ -1,10 +1,10 @@
-var exo5=document.getElementById("exo5");
+var exo5 = document.getElementById("exo5");
 exo5.addEventListener("click", eva5);
-function eva5(){
-    let multiplicateur=parseInt(prompt("entrer votre table de multiplication"));
-    let result=0
-    for(i=0;i<=10;i++){
-        result=i*multiplicateur;
-        console.log(i+" * "+multiplicateur+" = "+result);
+function eva5() {
+    let multiplicateur = parseInt(prompt("entrer votre table de multiplication"));
+    let result = 0
+    for (i = 0; i <= 10; i++) {
+        result = i * multiplicateur;
+        console.log(i + " * " + multiplicateur + " = " + result);
     }
 }
