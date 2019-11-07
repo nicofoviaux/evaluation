056e48787a3baf6c5b1df17dea0dce983d3319c6
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:14:51 +0100

    Create test.js

---
+++ js/assest/js/1ere partie/test.js
@@ -0,0 +1,8 @@
+var reponse1 = window.prompt("saisissez votre nom");// prompt pour prendre la variable//
+var reponse2 = window.prompt("saisissez votre prenom");
+// confirm pour demander une variable booleene//
+if (window.confirm("etes-vous un homme?") == true){
+       window.alert("bienvenue M "+reponse1+ " " +reponse2+"\n"+"bienvenue sur testouille" )
+      }// alert pour affiché//
+else
+      {window.alert("bienvenue Mme "+reponse1+" "+reponse2+"\n"+"bienvenue sur testouille" )}
