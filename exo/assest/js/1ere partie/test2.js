c1e9190e88bb0d17ea5cdf353394d37249e48782
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:27:46 +0100

    Create test2.js

---
+++ exo/assest/js/1ere partie/test2.js
@@ -0,0 +1,9 @@
+var A = "100";//variable en chaine de caractére
+var B = 100;//variable en entier
+var C = 1.00;// variable en numerique
+var D = true;// variable en booleen
+
+prompt("ceci est une chaine de caracteres" + " " + A);
+B = B--;
+C = C + A;
+D = !true;
