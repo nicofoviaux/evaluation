c1bc554e8559fa1df028e4c5b9cb2be1c2039e01
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:28:10 +0100

    Create test4.3.js

---
+++ exo/assest/js/1ere partie/test4.3.js
@@ -0,0 +1,14 @@
+var A = parseInt(prompt("ecrire un nombre"));
+var B = parseInt(prompt("ecrire un nombre supérieur au 1er"));
+var i = A
+var total = 0
+
+if (A > B) {// j'empeche l'utilisateur d'inversé le sens //
+    alert("ERROR");
+} else {
+    while (i <= B) {
+        total = total + i;
+        i++;
+    }
+} 
+alert(total);
