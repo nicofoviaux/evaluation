7c5e198dbfca986ae8b76d2970ba1eab8b8a7d41
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:16:47 +0100

    Create test5.4.js

---
+++ js/assest/js/1ere partie/test5.4.js
@@ -0,0 +1,5 @@
+function str(str1,str2,n){
+var word=str1.split(str2);//split separe les chaine de caractéres en tableau et utilise des separateur nommé entre () ici nommee par la variable str2qui est ";"
+  console.log(word[n-1]);
+}
+str( "robert;dupont;amiens;80000",";",3);
