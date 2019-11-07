5d65384a2bdd8c4d66b71e6646dc41ad988e4875
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:30:26 +0100

    Delete test6.1.js

--- js/assest/js/1ere partie/test6.1.js
+++
@@ -1,6 +0,0 @@
-var nbcol=parseInt(prompt("entrer le nombre de colones"));
-var table=Array(nbcol);
-for(i=0;i<=nbcol;i++){
-    table[i]=prompt("entrer le contenu de la case: "+i);
-}
-console.log(table)
