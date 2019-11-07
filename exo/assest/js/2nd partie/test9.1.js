3b4eabc35799bf0486600205cd43e8752447daa8
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:29:02 +0100

    Create test9.1.js

---
+++ exo/assest/js/2nd partie/test9.1.js
@@ -0,0 +1,17 @@
+var magic = parseInt(Math.random() * 100);
+var nb = document.getElementById("textBox1");//integre a la variabe nb l'element a l'id "textBox1" qui est le texte html
+var but = document.getElementById("button1");// integre a la variable but l'element pour voir si le bouton est cliqué (ici inutile car le bouton lance directement la fonction "verif")
+var text = document.getElementById("label1");// integre a la variable but l'element de l'id "label1" il sert ici a changer le text dans la div html
+//l'appel de la fonction verif ce fait dans le html
+function verif() {
+    if (nb.value < magic) {
+        text.innerHTML = "plus grand!!!"; //inner html sert a changer le texte sur le html ici on change la variable text
+    } else if (nb.value > magic) {
+        text.innerHTML = "plus petit!!!";
+    } else
+        text.innerHTML = "GAGNER!!!!!";
+    console.log(nb.value);
+    console.log(but);
+}
+console.log(magic);
+// pas besoin de faire de boucle la page se rafraichi a chague appuie sur le bouton
