47975a355f1983967260104372b309a67b0c9aaf
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:21:29 +0100

    Update exo3.js

--- assets/js/exo3.js
+++ assets/js/exo3.js
@@ -1,23 +1,23 @@
-var exo3=document.getElementById("exo3");
-exo3.addEventListener("click",eva3);
-function eva3 (){
-    let myTab=new Array();
-    let nb=Number;
-    i=0;
-    do { nb=parseInt(prompt("ecrire un chiffre tapez 0 pour arreter la saisie"))
+var exo3 = document.getElementById("exo3");
+exo3.addEventListener("click", eva3);
+function eva3() {
+    let myTab = new Array();
+    let nb = Number;
+    i = 0;
+    do {
+        nb = parseInt(prompt("ecrire un chiffre tapez 0 pour arreter la saisie"))
         myTab.push(nb);
         i++;
-    }while(nb!=0);
+    } while (nb != 0);
     myTab.pop();
-    var max=myTab.reduce(function (a,b){
-        return Math.max (a,b);
+    var max = myTab.reduce(function (a, b) {
+        return Math.max(a, b);
     });
-    var min=myTab.reduce(function(c,d){
-        return Math.min(c,d);
+    var min = myTab.reduce(function (c, d) {
+        return Math.min(c, d);
     });
-    alert("le nombre minimum saisie est: "+min+" et le nombre maximum saisie est: "+max);
+    alert("le nombre minimum saisie est: " + min + " et le nombre maximum saisie est: " + max);
     console.log(myTab);
-    console.log("max: "+max);
-    console.log("min: "+min);
-
+    console.log("max: " + max);
+    console.log("min: " + min);
 }
