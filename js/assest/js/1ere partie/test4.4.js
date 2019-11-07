35ea790cec7a4fcfe70c203c0320ef4bf0e3662b
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-07 11:16:25 +0100

    Create test4.4.js

---
+++ js/assest/js/1ere partie/test4.4.js
@@ -0,0 +1,17 @@
+var i=0
+var  A=parseInt(prompt("ecrire un nombre"))
+var B=A
+var total=0
+   while(B){
+       if(B==undefined){//undefined veut dire que si le champ est vide //
+        break;//j'arrete la boucle//
+       }
+       total=total+B;
+       B=parseInt(prompt("ecrire un nombre"));
+        i++;
+       
+    }
+    alert("total="+total+" "+"nb "+i);
+    var moy=total/i;
+    alert("moyénne= "+ moy );
+
